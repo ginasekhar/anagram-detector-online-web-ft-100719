b66e0010473423b6a18d02f58bc77fe56c3cb2e5
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-17 17:01:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,13 @@ class Anagram
   end #initialize
   
   def match(possible_anagrams)
+    sorted_original_word = split(word).sort.join
+    match_list = []
     same_length_anagrams = possible_anagrams.find_all {| word| word.length == @word.length}
     same_length_anagrams.each do |word_to_match|
-      split(word_to_match).sort.join
+      sorted_anagram = split(word_to_match).sort.join
+      if 
+      
     
     
   end
